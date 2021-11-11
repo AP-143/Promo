667e03d0e019f39dd587f5fea59ee872e41565a1
Akbar Putra <90847950+AP-143@users.noreply.github.com>
2021-11-11 09:13:40 +0700

    Add files via upload

---
+++ MainWindoController.cs
@@ -0,0 +1,36 @@
+﻿using penjualanMakanan.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penjualanMakanan.Controller
+{
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+
+    }
+
+}
